2fdcd9f8ab45759f05e054869549d6cd2d28aa0a
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-05-28 03:03:25 +0200

    Task 4 finished

---
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -0,0 +1,18 @@
+--#1 Select all students with all the available fields
+select * from student
+    ;
+
+--#2 Select 50 students
+select * from student limit 50
+    ;
+
+--#3 Select only students’ name
+select name from student
+    ;
+
+--#4 Select unique values of Amount from Orders table
+select distinct amount from payment
+    ;
+
+
+
