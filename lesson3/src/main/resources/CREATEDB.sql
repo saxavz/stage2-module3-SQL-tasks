5248947acd978e90737ee402cafb7bbec82df08d
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-05-28 02:58:42 +0200

    Task 3 finished

--- lesson3/src/main/resources/CREATEDB.sql
+++ lesson3/src/main/resources/CREATEDB.sql
@@ -2,7 +2,7 @@ CREATE TABLE student
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45),
-    birthday DATE,
+    birthday DATE NOT NULL,
     groupnumber INT NOT NULL
 );
 CREATE TABLE subject
@@ -10,17 +10,19 @@ CREATE TABLE subject
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(250),
     description VARCHAR(255),
-    grade int NOT NULL
+    grade INT NOT NULL,
+    CHECK (grade > 0 and grade <= 5)
 );
 
 CREATE TABLE mark
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    student_id BIGINT,
-    subject_id BIGINT,
-    mark INT,
+    student_id BIGINT NOT NULL,
+    subject_id BIGINT NOT NULL,
+    mark INT NOT NULL,
     foreign key (student_id) references student(id),
-    foreign key (subject_id) references subject(id)
+    foreign key (subject_id) references subject(id),
+    CHECK (mark > 0 and mark <= 10)
 );
 
 CREATE TABLE paymenttype
@@ -29,13 +31,18 @@ CREATE TABLE paymenttype
     name VARCHAR(45)
 );
 
+CREATE UNIQUE INDEX IDX_UNI_NAME_PAYMENTTYPE ON paymenttype
+  (
+    name
+  );
+
 CREATE TABLE payment
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    type_id BIGINT,
-    amount decimal,
-    payment_date DateTime,
-    student_id BIGINT NOT NULL,
+    type_id BIGINT NOT NULL,
+    amount decimal NOT NULL,
+    payment_date DateTime NOT NULL,
+    student_id  BIGINT NOT NULL ,
     foreign key (type_id) references paymenttype(id),
     foreign key (student_id) references student(id)
 );
