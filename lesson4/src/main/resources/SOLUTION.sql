8d12b06f382d35c433bf4378ccdc097b6289a3f8
Aliaksandr Hrakhouski <aliaksandr_hrakhouski@epam.com>
2023-05-28 03:39:45 +0200

    Task 4 sql formatting changed

--- lesson4/src/main/resources/SOLUTION.sql
+++ lesson4/src/main/resources/SOLUTION.sql
@@ -1,18 +1,10 @@
---#1 Select all students with all the available fields
-select * from student
-    ;
+select * from student;
 
---#2 Select 50 students
-select * from student limit 50
-    ;
+select * from student limit 50;
 
---#3 Select only students’ name
-select name from student
-    ;
+select name from student;
 
---#4 Select unique values of Amount from Orders table
-select distinct amount from payment
-    ;
+select distinct amount from payment;
 
 
 
